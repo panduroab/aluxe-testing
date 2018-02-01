c66724977ac1bb0cb014e2a42ce48b97e8fee471
Abraham Panduro <panduroab@gmail.com>
2018-02-01 12:46:58 -0600

    first commit

---
+++ tests/lib/index.expect.js
@@ -0,0 +1,21 @@
+describe("Aluxe Testing library", () => {
+    it("should generate the testing.ecosystem.js file", done => {
+
+    });
+
+    it("should get all the entities from a testing.ecosystem.js file", done => {
+
+    });
+
+    it("should create test files by template", done => {
+
+    });
+
+    it("should read the Dockerfiles for each project", done => {
+
+    });
+
+    it("should setup the docker environment by using the Dockerfiles", done => {
+
+    });
+});
